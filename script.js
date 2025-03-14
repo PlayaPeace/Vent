8db6531d04359c924d593d28244a1ae8a7e5daab
PlayaPeace <132778200+PlayaPeace@users.noreply.github.com>
2025-03-15 00:15:31 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -61,9 +61,6 @@ let inputs = document.querySelectorAll('input');
 
 for (let elem = 0; elem < inputs.length; elem++){
    inputs[elem].addEventListener('keydown', function (event) {
-	   setTimeout(() => {
-  calculate(formulaResult0, hiddenFormulaCont, formulaCont, canvasCont);
-}, 500); // Задержка 500 мс для инициализации MathJax
       if (event.key === 'Enter') {
          const target = event.target;
          let maths = document.querySelectorAll('math');
@@ -422,13 +419,8 @@ function calculate(formulaResult, hiddenFormulaCont, formulaCont, canvasCont) {
   var hiddenFormulaDiv = document.getElementById(hiddenFormulaCont);
   
   hiddenFormulaDiv.innerHTML = `$$${formulaResult}$$`;
- MathJax.typesetPromise([hiddenFormulaDiv])
-    .then(() => {
-      return MathJax.typesetPromise([hiddenFormulaDiv]); // Двойная проверка
-    })
-    .then(() => {
-      const svg = hiddenFormulaDiv.querySelector('svg');
-      if (!svg) throw new Error('SVG element not found');
+  MathJax.typesetPromise([hiddenFormulaDiv]).then(() => {
+    var svg = hiddenFormulaDiv.querySelector('svg');
     var canvas = document.getElementById(canvasCont);
     if (svg) {
       var xml = new XMLSerializer().serializeToString(svg);
